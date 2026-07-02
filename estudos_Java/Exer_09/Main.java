868b100449d5f1657d799b311a8cbefd57ffd505
EnzoBerriel <enzoberriel1@gmail.com>
2026-07-02 10:55:49 -0300

    +java Exercise

--- estudos_Java/Exer_09/Main.java
+++ estudos_Java/Exer_09/Main.java
@@ -15,6 +15,45 @@ Exiba os resultados no formato: "Aluno 1: média 7.33 - Aprovado".
 
 package estudos_Java.Exer_09;
 
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        //Variaveis
+        String[] nome = new String[4];
+        Double[][] matriz = new Double[4][3];
+        Double[] media = new Double[4];
+        Double tot;
+        //Abrindo Scanner
+        Scanner rl = new Scanner(System.in);
+        //Requerimentos
+        for(int i=0;i<4;i++){
+            System.out.printf("Digite o Nome do %d Aluno:",i+1);
+            nome[i] = rl.nextLine();
+            tot = 0.0;
+            for(int c=0;c<3;c++){
+                System.out.printf("%d Nota: ",c+1);
+                matriz[i][c] = rl.nextDouble();
+                tot+=matriz[i][c];
+            }
+            //Media
+            media[i] = tot/3;
+            //Buffer
+            rl.nextLine();
+        }
+        //Fechando Scanner
+        rl.close();
+        //Resposta
+        for (int i=0;i<4;i++){
+            System.out.printf("Aluno: %s\nMédia: %.2f\n",nome[i],media[i]);
+            if(media[i] >=6){
+                System.out.printf("Situação: Aprovado");
+            }
+            else{
+                System.out.printf("Situação: Reprovado");
+            }
+            System.out.printf("\n\n");
+        }
+
+    }
 }
